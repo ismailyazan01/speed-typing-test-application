d4cf1c2bddce27ce66b58678511017aa2fbd4113
ismailyazan01 <ismailyazan01@gmail.com>
2023-12-05 00:46:51 -0800

    Changed words per minute accuracy from word to word accuracy to letter to letter accuracy. Also, added documentation.

--- speed_typing_application.py
+++ speed_typing_application.py
@@ -3,15 +3,28 @@ import time
 
 
 class TypingQuiz:
-    def __init__(self, startTime=None, testTime=15, wordsTotal=0, wordsCorrect=0):
+    def __init__(self, startTime=None, testTime=30, charsTotal=0, charsCorrect=0):
+        """Initialize TypingQuiz object.
+
+        Parameters:
+        - startTime (float): The start time of the quiz, default is None.
+        - testTime (int): The duration of the typing test in seconds, default is 30 seconds.
+        - charsTotal (int): Total number of characters typed by the user, default is 0.
+        - charsCorrect (int): Number of correctly typed characters, default is 0.
+        """
         if startTime is None:
             startTime = time.time()
         self.elapsedTime = time.time() - startTime
         self.testTime = testTime
-        self.wordsTotal = wordsTotal
-        self.wordsCorrect = wordsCorrect
+        self.charsTotal = charsTotal
+        self.charsCorrect = charsCorrect
 
     def startUpMenu(self):
+        """Display the welcome message and get the difficulty level from the user.
+
+        Returns:
+        - int: Difficulty level chosen by the user (1 for Easy, 2 for Medium, 3 for Hard).
+        """
         print("Welcome to the Speed Typing Test!")
         while True:
             try:
@@ -26,6 +39,7 @@ class TypingQuiz:
                 print("Invalid input. Please enter a valid integer.\n", ve)
 
     def runTest(self):
+        """Run the typing test based on user input and display the results."""
         difficulty = self.startUpMenu()
         randomTestNumber = random.randint(1, 5)
         if difficulty == 1:
@@ -45,32 +59,41 @@ class TypingQuiz:
         print("\nTest started!\n")
 
         self.printPassage(testFilePath)
-        wordsPerMinute = round(self.wordsCorrect / self.testTime, 2) * 60
-
-        print(wordsPerMinute)
-        print(self.wordsTotal)
+        accuracy = str(round(self.charsCorrect / self.charsTotal, 4) * 100) + "%"
+        wordsPerMinute = round((self.charsTotal / 5) / (self.elapsedTime / 60), 2)
         print(self.elapsedTime)
-
-        print(f"You typed {wordsPerMinute} words per minute!")
+        print(f"You typed {wordsPerMinute} words per minute with {accuracy} accuracy!")
+        if input("\nWould you like to play again?\nType 1 to play again\nType 2 to exit\n") == str(1):
+            self.charsTotal = 0
+            self.charsCorrect = 0
+            self.runTest()
 
     def printPassage(self, testFilePath):
+        """Display the passage for the typing test and record user input.
+
+        Parameters:
+        - testFilePath (str): File path to the text passage for the typing test.
+        """
         startTime = time.time()
         self.elapsedTime = time.time() - startTime
         with open(testFilePath, 'r') as file:
             for line in file:
-                userWordIndex = 0
+                userLetterIndex = 0
                 self.elapsedTime = time.time() - startTime
                 if self.elapsedTime >= self.testTime:
                     return
                 print(line)
-                usersLine = input().split()
-                for word in line.split():
-                    if userWordIndex < len(usersLine) and word == usersLine[userWordIndex]:
-                        self.wordsCorrect += 1
-                    userWordIndex += 1
-                    self.wordsTotal += 1
+                usersLine = input()
+                for char in line:
+                    if userLetterIndex < len(usersLine):
+                        if char == usersLine[userLetterIndex]:
+                            self.charsCorrect += 1
+                        userLetterIndex += 1
+                        self.charsTotal += 1
 
 
+# Instantiate the TypingQuiz class
 TypingQuizInstance = TypingQuiz()
 
+# Run the typing test
 TypingQuizInstance.runTest()
